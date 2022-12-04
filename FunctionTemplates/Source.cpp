610e8c59080c987f41c623afc972703af9d24466
katherWal <katherine.walsh@sinclair.edu>
2022-12-04 11:36:38 -0500

    Add project files.

---
+++ FunctionTemplates/Source.cpp
@@ -0,0 +1,19 @@
+/*
+Katherine Walsh
+Template Functions
+12/4/22
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	double a = 7.0;
+	float b = 5.0f;
+	int c = 3;
+
+
+	cout << endl;
+	system("pause");
+	return 0;
+}
